ce4da8a4c6982b041aff82f0961abb86b904cca2
Edgar Vergara <edgarvm13@hotmail.com>
2023-10-14 08:22:06 +0200

    project setup: models

---
+++ backend/src/main/java/com/vergaraaa/backend/model/User.java
@@ -0,0 +1,30 @@
+package com.vergaraaa.backend.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+    @NotBlank(message = "Username is required≤")
+    private String username;
+    @NotBlank(message = "Password is required≤")
+    private String password;
+    @NotBlank(message = "Email is required≤")
+    private String email;
+    private Instant created;
+    private boolean enabled;
+}
